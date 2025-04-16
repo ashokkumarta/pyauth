e21258a2dbbd592c22c39405646097ea4bde4180
Ashokkumar T.A. <ashokkumar.ta@gmail.com>
2025-04-16 14:57:50 +0530

    Initial version

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='pyauth',
+   version='0.1',
+   description='Enfore simple access control policy for REST API',
+   author='Ashokkumar T.A',
+   author_email='foomail@foo.example',
+   packages=['pyauth'],  
+   install_requires=[], 
+   keywords = ['Auth', 'Python library', 'Python tool', 'Access', 'Authorization', 'Policy'], 
+)
