567b00cc9db431016e055ce34b4553b7bcb31ca2
Ashokkumar T.A. <ashokkumar.ta@gmail.com>
2025-04-22 10:39:16 +0530

    Updated version, Security flag and separate role and data access check classes

--- pyauth/auth.py
+++ pyauth/auth.py
@@ -1,24 +1,70 @@
 from rest_framework import permissions
-from .jwt_check import checkAccess
+from .jwt_check import *
+
+AUTH_USERID_KEY =  'auth-user-id'
+AUTH_BRANCH_CODE_KEY =  'auth-branch-code'
 
 class HasRoleAndDataPermission(permissions.BasePermission):
-    """
-    Global permission check for blocked IPs.
-    """
-
-    def has_permission(self, request, view):
+    def has_permission(self, request, view) -> bool:
+        if isSecurityDisabled():
+            print(f'WARNING: Security disabled in this environment [HasRoleAndDataPermission check skipped]\n')
+            return True
         try:
             token = request.headers["Authorization"]
             branch_code = request.headers["Branch-Code"]
             page_path = request.get_full_path()
             http_method = request.method
-            checkAccess(token, branch_code, page_path, http_method)
-            print(f'Access allowed\n')
+            vjson = checkAccess(token, branch_code, page_path, http_method)
+            request.headers[AUTH_USERID_KEY] = vjson[AUD_KEY]
+            request.headers[AUTH_BRANCH_CODE_KEY] = branch_code
+            print(f'HasRoleAndDataPermission: Access allowed\n')
             return True
         except ValueError as e:
-            print(f'Access not allowed. Reason: {e}\n')
+            print(f'HasRoleAndDataPermission: Access not allowed. Reason: {e}\n')
             return False
         except:
-            print(f'Error occured in access validation')
+            print(f'HasRoleAndDataPermission: Error occured in access validation')
             return False
+
+class HasDataPermission(permissions.BasePermission):
+    def has_permission(self, request, view) -> bool:
+        if isSecurityDisabled():
+            print(f'WARNING: Security disabled in this environment [HasDataPermission check skipped]\n')
+            return True
+        try:
+            token = request.headers["Authorization"]
+            branch_code = request.headers["Branch-Code"]
+            vjson = checkAccessForData(token, branch_code)
+            request.headers[AUTH_USERID_KEY] = vjson[AUD_KEY]
+            request.headers[AUTH_BRANCH_CODE_KEY] = branch_code
+            print(f'HasDataPermission: Access allowed\n')
+            return True
+        except ValueError as e:
+            print(f'HasDataPermission: Access not allowed. Reason: {e}\n')
+            return False
+        except:
+            print(f'HasDataPermission: Error occured in access validation')
+            return False
+
+class HasRolePermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if isSecurityDisabled():
+            print(f'WARNING: Security disabled in this environment [HasRolePermission check skipped]\n')
+            return True
+        try:
+            token = request.headers["Authorization"]
+            page_path = request.get_full_path()
+            http_method = request.method
+            vjson = checkAccessForPageAction(token, page_path, http_method)
+            request.headers[AUTH_USERID_KEY] = vjson[AUD_KEY]
+            request.headers[AUTH_BRANCH_CODE_KEY] = ""
+            print(f'HasRolePermission: Access allowed\n')
+            return True
+        except ValueError as e:
+            print(f'HasRolePermission: Access not allowed. Reason: {e}\n')
+            return False
+        except:
+            print(f'HasRolePermission: Error occured in access validation')
+            return False
+
 
--- pyauth/jwt_check.py
+++ pyauth/jwt_check.py
@@ -8,6 +8,7 @@ from types import SimpleNamespace
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 
+SECURITY_DISABLED_FLAG =  'SECURITY_DISABLED'
 PUBLIC_KEY_NAME =  'GLOBAL_PUBLIC_KEY'
 ISSUER_KEY =  'iss'
 ISSUER_VALUE =  'https://lab.shinova.in/'
@@ -21,31 +22,64 @@ ALLOWED_ACTIONS_KEY = 'allowed-actions'
 AUTHZ_MODEL = 'IMPLIED'
 AUTHZ_MODEL_IMPLIED = 'IMPLIED'
 
-_pubk_B64 = os.environ[PUBLIC_KEY_NAME]
-if not _pubk_B64:
-   raise ValueError(f'Private key ({PUBLIC_KEY_NAME}) not set')
+__sec_dsiabled_flag = os.environ[SECURITY_DISABLED_FLAG].lower()
+SECURITY_DISABLED = (__sec_dsiabled_flag == 'true' or __sec_dsiabled_flag == 'yes' or __sec_dsiabled_flag == 'y')
 
-_pubk = base64.b64decode(_pubk_B64) 
+if not SECURITY_DISABLED:
+   _pubk_B64 = os.environ[PUBLIC_KEY_NAME]
+   if not _pubk_B64:
+      raise ValueError(f'Private key ({PUBLIC_KEY_NAME}) not set')
 
-PUBLIC_KEY = serialization.load_pem_public_key(
-   _pubk, backend=default_backend())
+   _pubk = base64.b64decode(_pubk_B64) 
 
+   PUBLIC_KEY = serialization.load_pem_public_key(
+      _pubk, backend=default_backend())
 
-def load_module_from_file(file_path):
+def __load_module_from_file(file_path):
    module_namespace = {}
    with open(file_path, 'r') as file:
       exec(file.read(), module_namespace)
    return module_namespace
 
-def find_file_by_name(file_name, search_path):
+def __find_file_by_name(file_name, search_path):
     for root, dirs, files in os.walk(search_path):
         if file_name in files:
             return os.path.join(root, file_name)
     return None
 
-file_path = find_file_by_name("permissions_map.py", ".")
+file_path = __find_file_by_name("permissions_map.py", ".")
 print(f"Loading permissions from: {file_path}")
-permissions = load_module_from_file(file_path)
+permissions = __load_module_from_file(file_path)
+
+def isSecurityDisabled():
+   return SECURITY_DISABLED
+
+
+def checkAccess(accessToken:str, 
+                data:str, 
+                page:str, 
+                action:str) -> dict:
+
+   vJson = __checkJwt(accessToken)
+   __checkAccessForData(vJson, data)
+   __checkAccessForPageAction(vJson, page, action)
+   return vJson
+
+def checkAccessForData(accessToken:str, 
+                data:str) -> dict:
+
+   vJson = __checkJwt(accessToken)
+   __checkAccessForData(vJson, data)
+   return vJson
+
+
+def checkAccessForPageAction(accessToken:str, 
+                page:str, 
+                action:str) -> dict:
+
+   vJson = __checkJwt(accessToken)
+   __checkAccessForPageAction(vJson, page, action)
+   return vJson
 
 def __checkJwt(accessToken:str):
 
@@ -64,20 +98,22 @@ def __checkJwt(accessToken:str):
    verified = jwt.decode(accessToken, key=_pubk, algorithms="RS256", audience=tokenAud)
    return verified
 
-def checkAccess(accessToken:str, 
-                data:str, 
-                page:str, 
-                action:str) -> dict:
-
-   vJson = __checkJwt(accessToken)
+def __checkAccessForData(vJson:dict, 
+                data:str) -> bool:
 
    # Data validation
    if data not in vJson[ALLOWED_DATA_KEY]:
       raise ValueError(f'Access denied [Not entitled to access requested data {data}]', data)
+   return True
+
+
+def __checkAccessForPageAction(vJson:dict, 
+                page:str, 
+                action:str) -> bool:
 
    pageId = permissions.PAGE_MAPPING[page]
    if not pageId:
-      raise ValueError(f'Access denied [Invalid page]')
+      raise ValueError(f'Access denied [Invalid page]: {pageId}')
 
    if pageId in permissions.PAGE_ACTION_MAPPING and action in permissions.PAGE_ACTION_MAPPING[pageId]:
       actionId = permissions.PAGE_ACTION_MAPPING[pageId][action]
@@ -85,7 +121,7 @@ def checkAccess(accessToken:str,
       actionId = permissions.GEN_ACTION_MAPPING[action]
 
    if not actionId:
-      raise ValueError(f'Access denied [Invalid action]')
+      raise ValueError(f'Access denied [Invalid action]: {actionId}')
 
    permissionId = pageId + '-' + actionId    
 
@@ -100,4 +136,5 @@ def checkAccess(accessToken:str,
          raise ValueError(f'Access denied [Not allowed to perform {action} on {page}]',action, page)
 
    # Allowed access
-   return vJson
+   return True
+
