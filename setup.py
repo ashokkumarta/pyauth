99393ef438f92d7f27532220ec2e949c7f774a0b
Ashokkumar T.A. <ashokkumar.ta@gmail.com>
2026-03-23 19:17:37 +0530

    Update dependencies for the library

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-import requests
 from setuptools import setup
 
 setup(
    name='pyauth',
-   version='1.2.7',
+   version='1.2.8',
    description='Enfore simple access control policy for REST API',
    author='Ashokkumar T.A',
    author_email='ashokkumar.ta@gmail.com',
    packages=['pyauth'],  
-   install_requires=[requests], 
+   install_requires=["requests==2.32.5"],
    keywords = ['Auth', 'Python library', 'Python tool', 'Access', 'Authorization', 'Policy'], 
 )
