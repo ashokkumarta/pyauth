0af2fa780da714bff82c5cd7f3bf143f110d7403
Ashokkumar T.A. <ashokkumar.ta@gmail.com>
2026-04-17 13:31:12 +0530

    Update setup data

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
    name='pyauth',
-   version='1.2.11',
-   description='Enfore simple access control policy for REST API',
+   version='1.3.1-rl',
+   description='Enfore simple access control policy for REST API, and also support rate limit',
    author='Ashokkumar T.A',
    author_email='ashokkumar.ta@gmail.com',
    packages=['pyauth'],  
-   install_requires=["requests==2.32.5"],
-   keywords = ['Auth', 'Python library', 'Python tool', 'Access', 'Authorization', 'Policy'], 
+   install_requires=["requests==2.32.5", "pyrate_limiter==4.1.0"],
+   keywords = ['Auth', 'Python library', 'Python tool', 'Access', 'Authorization', 'Policy', 'rate limit'], 
 )
